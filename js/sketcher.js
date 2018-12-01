ef9b3c45f0ef70b4e736e4eedd89657ad5b8deba
Victor Chen <chenvictor1999@gmail.com>
2018-12-01 14:48:58 -0800

    Added sketcher js

---
+++ js/sketcher.js
@@ -0,0 +1,104 @@
+const CANVAS = document.getElementById("canvas");
+let rect = CANVAS.getBoundingClientRect();
+CANVAS.width = rect.width;
+CANVAS.height = rect.height;
+let LEFT = rect.left;
+let TOP = rect.top;
+
+console.log(CANVAS.getBoundingClientRect());
+const Sketcher = new function() {
+    const STROKE_SIZE = 4;
+    const THRESHOLD = 5;   // Dist at which a new point will be added
+    let ctx = CANVAS.getContext('2d');
+    ctx.lineWidth = STROKE_SIZE;
+    let strokes = [];
+    let currentStroke = 0;
+    let isMouseDown = false;
+    this.startStroke = function(event) {
+        if (!isMouseDown) {
+            isMouseDown = true;
+            strokes.push([getPoint(event)]);
+        }
+    };
+    this.endStroke = function() {
+        if (isMouseDown) {
+            isMouseDown = false;
+            if (strokes[currentStroke].length > 1) {
+                currentStroke++;
+            } else {
+                strokes.pop();
+            }
+        }
+    };
+    this.moveStroke = function(event) {
+        if (isMouseDown) {
+            let point = getPoint(event);
+            let current = strokes[currentStroke];
+            let lastPoint = current[current.length - 1];
+            if (dist(point, lastPoint) > THRESHOLD) {
+                ctx.beginPath();
+                ctx.moveTo(lastPoint[0], lastPoint[1]);
+                strokes[currentStroke].push(point);
+                ctx.lineTo(point[0], point[1]);
+                ctx.stroke();
+            }
+        }
+    };
+    this.clear = function() {
+        strokes = [];
+        currentStroke = 0;
+        isMouseDown = false;
+        draw();
+    };
+    this.undo = function() {
+        if (strokes.length > 0) {
+            strokes.pop();
+            currentStroke--;
+            draw();
+        }
+    };
+    let draw = function() {
+        ctx.clearRect(0, 0, CANVAS.width, CANVAS.height);
+        for (let i = 0; i < strokes.length; i++) {
+            // Draw a stroke
+            let stroke = strokes[i];
+            ctx.beginPath();
+            ctx.moveTo(stroke[0][0], stroke[0][1]);
+            for (let j = 1; j < stroke.length; j++) {
+                ctx.lineTo(stroke[j][0], stroke[j][1]);
+            }
+            ctx.stroke();
+        }
+    };
+
+    let getPoint = function(event) {
+        return [event.pageX - LEFT, event.pageY - TOP];
+    };
+
+    let dist = function(point1, point2) {
+        let dx = point1[0] - point2[0];
+        let dy = point1[1] - point2[1];
+        return Math.sqrt(dx*dx + dy*dy);
+    };
+};
+
+// Register listeners
+CANVAS.addEventListener("mousedown", Sketcher.startStroke);
+CANVAS.addEventListener("mouseup", Sketcher.endStroke);
+CANVAS.addEventListener("mouseleave", Sketcher.endStroke);
+CANVAS.addEventListener("mousemove", Sketcher.moveStroke);
+
+CANVAS.addEventListener("touchstart", Sketcher.startStroke);
+CANVAS.addEventListener("touchend", Sketcher.endStroke);
+CANVAS.addEventListener("touchcancel", Sketcher.endStroke);
+CANVAS.addEventListener("touchmove", Sketcher.moveStroke);
+
+// Key listeners
+window.addEventListener("keypress", (e) => {
+    console.log(e.key);
+    if (e.key === 'z') {
+        Sketcher.undo();
+    } else if (e.key === 'x') {
+        Sketcher.clear();
+    }
+});
